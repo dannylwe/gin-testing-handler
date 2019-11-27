00d28efaec3f34ae2751d91841dd0b7d8cb07366
Daniel Lwetabe <acehanks@gmail.com>
2019-11-27 16:39:54 +0300

    refactor

--- main.go
+++ main.go
@@ -5,12 +5,23 @@ import (
 	"net/http"
 )
 
+var PORT string
+
+func init(){
+	PORT = "9005"
+}
+
 func main() {
+	r := setupRouter()
+	r.Run(":" + PORT) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run(":9005") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	return r
 }
